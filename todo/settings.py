31d124867eb53b32b8ae8ac102de0fa6ec070abf
Pratik Chemate <150340799+pratikchemate@users.noreply.github.com>
2025-04-26 17:26:25 +0530

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-i-fn-%q0oey@nun*)$q_!w3ibd(eevrfw4l_+-)@-br6oux3r&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [Django-todo-1.com]
 
 
 # Application definition
